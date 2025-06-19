d4e9082aa0150a4c72147d1592347dd3f162fdee
Aaron Yang <aaronyang470128@hotmail.com>
2025-06-19 16:55:53 -0400

    Initial Commit

---
+++ WhishperLock/WhishperLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhishperLockApp.swift
+//  WhishperLock
+//
+//  Created by Aaron Yang on 2025-06-19.
+//
+
+import SwiftUI
+
+@main
+struct WhishperLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
